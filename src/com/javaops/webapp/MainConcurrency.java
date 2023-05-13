1cb7f07593633bbb55a270ab59fcaf024f934b56
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2023-05-13 19:06:46 +0700

    Deadlock

--- src/com/javaops/webapp/MainConcurrency.java
+++ src/com/javaops/webapp/MainConcurrency.java
@@ -3,34 +3,18 @@ package com.javaops.webapp;
 public class MainConcurrency {
     public static final int THREAD_NUMBER = 10000;
     private static int counter;
-    private static final Object LOCK1 = new Object();
-    private static final Object LOCK2 = new Object();
+    private static final String LOCK1 = "LOCK1";
+    private static final String LOCK2 = "LOCK2";
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName());
-        Thread thread = new Thread(() -> {
+//        System.out.println(Thread.currentThread().getName());
+//        Thread thread0 = new Thread() {
+//            @Override
+//            public void run() {
 //                System.out.println(getName() + ", " + getState());
-            System.out.println("thread: Пытаемся захватить LOCK1");
-            synchronized (LOCK1){
-                System.out.println("thread: LOCK1 захвачен");
-                System.out.println("thread: Пытаемся захватить LOCK2");
-                synchronized (LOCK2){
-                    System.out.println("thread: LOCK1 и LOCK2 захвачены");
-                }
-            }
-        });
-        Thread thread1 = new Thread(() -> {
-            System.out.println("thread1: Пытаемся захватить LOCK2");
-            synchronized (LOCK2){
-                System.out.println("thread1: LOCK2 захвачен");
-                System.out.println("thread1: Пытаемся захватить LOCK1");
-                synchronized (LOCK1){
-                    System.out.println("thread1: LOCK1 и LOCK2 захвачены");
-                }
-            }
-        });
-        thread.start();
-        thread1.start();
+//            }
+//        };
+//        thread0.start();
 //        new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
 //        List<Thread> threadList = new ArrayList<>(THREAD_NUMBER);
 //        System.out.println(thread.getState());
@@ -57,7 +41,24 @@ public class MainConcurrency {
 ////        synchronized (LOCK) {
 //            counter++;
 //        }
+//    }
+        //DeadLock
+       deadLock(LOCK1,LOCK2);
+       deadLock(LOCK2,LOCK1);
+
     }
-    //DeadLock
+    private static void deadLock(Object LOCK1, Object LOCK2){
+       Thread thread =  new Thread(()->{
+            System.out.println( Thread.currentThread().getName() + " : Пытаемся захватить " + LOCK1 );
+            synchronized (LOCK1){
+                System.out.println(Thread.currentThread().getName() + " : " +  LOCK1 + " захвачен ");
+                System.out.println(Thread.currentThread().getName() + " : Пытаемся захватить " + LOCK2);
+                synchronized (LOCK2){
+                    System.out.println("LOCK1 и LOCK2 захвачены");
+                }
+            }
+        });
+       thread.start();
+    }
 
 }
